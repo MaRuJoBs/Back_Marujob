d7cdac3b02d32d2e02c4087bb03103d5a943f960
RicbaronR <ric.baron01@gmail.com>
2026-08-26 20:27:31 -0300

    Add tipo do usuário

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -15,9 +16,24 @@ class UserSerializer(ModelSerializer):
 class UserRegistrationSerializer(ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
+    tipo = serializers.ChoiceField(
+        choices=[
+            ('usuario', 'Usuário'),
+            ('empresa', 'Empresa'),
+        ],
+        write_only=True
+    )
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'password', 'profile_image']
+        fields = ['id', 'email', 'name', 'password', 'profile_image', 'tipo']
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        tipo = validated_data.pop('tipo')
+        user = User.objects.create_user(**validated_data)
+        if tipo == 'empresa':
+            grupo = Group.objects.get(name='Empresa')
+        else:
+            grupo = Group.objects.get(name='Usuário')
+        user.groups.add(grupo)
+        
+        return user
